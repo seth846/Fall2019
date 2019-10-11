1843fedf5129ba8f536ab9b22793b480cf6a091c
seth846 <telumda3rd@gmail.com>
2019-10-11 15:21:55 -0500

    Finished Review

--- Chapter10/Programming_Assignment/store_temps.cpp
+++ Chapter10/Programming_Assignment/store_temps.cpp
@@ -1,10 +1,20 @@
 #include "std_lib_facilities.h"
-
+struct Reading {
+	int rand() hour;
+	int rand() temp;
+};
 int main()
 try{
-
-
-	return 0;		
+	int hour;
+	int temp;
+	string oname;
+	cout << "enter name of file: ";
+	cin >> oname;
+	ostream ost(oname);
+	if(!ost) error("can't open file ", oname);
+	for( int i =0; i < 55; ++i){
+		ost << Reading
+	}
 }
 catch(exception& e){
         cerr << e.what() << endl;
