2d9e30872ddc9863e288560fec41a03d9f932b6c
kyle <williams.kyle@gmx.com>
2019-10-20 17:02:23 -0500

    Added whitespace and adjusted comments in concatinate

--- Chapter10/Programming_Assignment/concatinate.cpp
+++ Chapter10/Programming_Assignment/concatinate.cpp
@@ -2,32 +2,50 @@
 
 int main()
 try{
+	//recieve first file from user
 	string iname1;
-	string iname2;
-	cout << "which file would you like to use first: ";		//recieve first file from user
+	cout << "which file would you like to use first: ";		
 	cin >> iname1;
-	cout << "which file would you like to use second: ";		//recieve second file from user
+
+	//recieve second file from user
+	string iname2;
+	cout << "which file would you like to use second: ";		
 	cin >> iname2;
+
+	//open first file for reading
+	ifstream ist(iname1);					
+	if(!ist) error("can't open file ", iname1);
+	
+	//add input from first file to 'files'
 	vector <string> files;					
 	string input;
-	ifstream ist(iname1);					//open first file for reading
-	if(!ist) error("can't open file ", iname1);
-	while(ist >> input){					//add input from first file to 'files'
+	while(ist >> input){					
 		files.push_back(input);
 	}
-	ifstream ifs(iname2);					//open second file for reading
+	
+	//open second file for reading
+	ifstream ifs(iname2);					
 	if(!ifs) error("can't open file ", iname2);
-	while(ifs >> input){					//add input from second file to 'files'
+
+	//add input from second file to 'files'
+	while(ifs >> input){					
 		files.push_back(input);
 	}
-	string oname;						//get file for writing from user
+
+	//get file for writing from user
+	string oname;						
 	cout << "where would you like to combine the files: ";
 	cin >> oname;
-	ofstream ost(oname);					//open 'oname' for writing
+
+	//open 'oname' for writing
+	ofstream ost(oname);					
 	if(!ost) error("can't open file ", oname);
-	for(int i = 0; i < files.size(); ++i){			//write each element in 'files' to 'oname'
+
+	//write each element in 'files' to 'oname'
+	for(int i = 0; i < files.size(); ++i){			
 		ost << files[i] << " ";
 	}
+	
 	cout << "complete" << "\n";			//print complete	
 	return 0;		
 }
