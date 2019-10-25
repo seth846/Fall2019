d9cac35cb4ed448db229c9449a7637cafa0bedab
seth846 <telumda3rd@gmail.com>
2019-10-25 15:51:26 -0500

    completed Drill

--- Chapter11/Programming_Assignment/problem1.cpp
+++ Chapter11/Programming_Assignment/problem1.cpp
@@ -2,7 +2,27 @@
 
 int main()
 try{
-
+	vector <string> file;
+	cout << "enter name of input file: ";
+	string iname;
+	cin >> iname;
+	ifstream ifs(iname);
+	if(!ifs) error("can't open file ", iname);
+	while(ifs){
+		string word;
+		ifs >> word;
+		file.push_back(word);
+	}
+	for(int i = 0; i < file.size(); ++i){
+		string word;
+		word = file[i];
+		for(int n = 0; n < word.size(); ++i){
+			if(word[n] == 'a' || word[n] == 'e'|| word[n] == 'i'||word[n] == 'o'||word[n] == 'u'){
+				delete[word[n]];	
+			}
+			else;	
+		}
+	}
 
 	return 0;		
 }
